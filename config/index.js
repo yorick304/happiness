0434051fa5c6fcafb4a8f12f7dc766da9785c839
杨宇亮 <yangyuliang@weidian.com>
2018-08-16 16:19:18 +0800

    update

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/happiness/dist',
 
     /**
      * Source Maps
