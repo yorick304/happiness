50a3f871229bde84bf66788a5b358710d395d8ee
杨宇亮 <yangyuliang@weidian.com>
2018-08-18 08:05:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -9,37 +9,36 @@ import {Store} from './store/index.js'
 export default {
   name: 'App',
   created() {
-    setTimeout(() => {
-      Store.fetchSignture({ url: 'http://fly.viicb.com/wechat/api/service/getWeChatSignature', param: {link: window.location.href.split('#')[0] }}).then((res) => {
-        console.log(res)
-        let data = res.data
-        let wxAccountInfo = data && data.wxAccountInfo
-        console.log(wxAccountInfo)
-        wx.config({
-          debug: false,
-          appId: wxAccountInfo.appId,
-          timestamp: wxAccountInfo.timestamp,
-          nonceStr: wxAccountInfo.nonceStr,
-          signature: wxAccountInfo.signature,
-          jsApiList: [
-            'checkJsApi',
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'onMenuShareQQ'
-          ]
-        }); 
-        wx.ready(function () {
-          var shareData = {
-            title: '杭州区域产业新城',
-            link: window.location.href,
-            imgUrl: 'https://si.geilicdn.com/resource-211c0000016546f8a23e0a02853e-unadjust_100_100.png'
-          };
-          wx.onMenuShareAppMessage(shareData)
-          wx.onMenuShareTimeline(shareData)
-          wx.onMenuShareQQ(shareData)
-        });
+    Store.fetchSignture({ url: 'http://fly.viicb.com/wechat/api/service/getWeChatSignature', param: {link: window.location.href.split('#')[0] }}).then((res) => {
+      console.log(res)
+      let data = res.data
+      let wxAccountInfo = data && data.wxAccountInfo
+      console.log(wxAccountInfo)
+      wx.config({
+        debug: true,
+        appId: wxAccountInfo.appId,
+        timestamp: wxAccountInfo.timestamp,
+        nonceStr: wxAccountInfo.nonceStr,
+        signature: wxAccountInfo.signature,
+        jsApiList: [
+          'checkJsApi',
+          'onMenuShareTimeline',
+          'onMenuShareAppMessage',
+          'onMenuShareQQ'
+        ]
       })
-    }, 0)
+
+      wx.ready(function () {
+        var shareData = {
+          title: '杭州区域产业新城',
+          link: window.location.href,
+          imgUrl: 'https://si.geilicdn.com/resource-211c0000016546f8a23e0a02853e-unadjust_100_100.png'
+        };
+        wx.onMenuShareAppMessage(shareData)
+        wx.onMenuShareTimeline(shareData)
+        wx.onMenuShareQQ(shareData)
+      })
+    })
   }
 }
 </script>
