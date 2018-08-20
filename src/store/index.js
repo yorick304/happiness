c0c35ac5ee7bc268ab5f0a892850b3da80ea3cba
杨宇亮 <yangyuliang@weidian.com>
2018-08-20 23:58:45 +0800

    upda

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,6 @@ import qs from 'qs'
 let http = axios.create({
   withCredentials: true
 })
-
-// axios.interceptors.request.use(config => {
-//   config.headers = {
-//     'Content-Type': 'application/x-www-form-urlencoded'
-//   }
-//   return config
-// })
 export const Store = {
   async fetchSignture(ops) {
     let res = await axios.post(ops && ops.url, qs.stringify(ops.param))
