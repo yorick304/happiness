b4ea42b78e2b65e5cc1cd7e38b7cbc1b29284618
杨宇亮 <yangyuliang@weidian.com>
2018-08-20 23:09:21 +0800

    upda

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,22 @@ let http = axios.create({
 })
 export const Store = {
   async fetchSignture(ops) {
-    let res = await axios.post(ops && ops.url, ops.param)
+    // let res = await axios.post(ops && ops.url, ops.param)
+    let res = await http({
+      url: ops && ops.url,
+      method: 'post',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      },
+      transformRequest: [function(data) {
+        let ret = ''
+        for (let it in data) {
+          ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+        }
+        return ret
+      }],
+      data: ops && ops.param
+    })
     return res.data
   }
 }
